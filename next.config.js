a4fc330097af085f21f3aef95ee0a8b3b1860770
Agustin Diaz <agustin.ramiro.diaz@gmail.com>
2021-12-18 19:33:45 -0300

    Gh deploy test

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   reactStrictMode: true,
+  basePath: !debug ? '/calles_de_rosario' : '',
   assetPrefix: !debug ? '/calles_de_rosario/' : '',
 }
